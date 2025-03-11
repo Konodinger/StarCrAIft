fa482e222cd9e0c1b56c4f72d8f5ba7408596e75
enzo.gelas <enzo.gelas@telecom-paris.fr>
2025-03-11 17:33:16 +0100

    First implementation ScoutTask

--- src/starterbot/UtilitySystem/ScoutTask.cpp
+++ src/starterbot/UtilitySystem/ScoutTask.cpp
@@ -1,2 +1,17 @@
 #include "ScoutTask.h"
 
+ScoutTask::ScoutTask(int& enemyBasePosListSize) : Task("Scout") {
+	m_taskBT = std::make_shared<BT_NODE>(BT_DECORATOR("EntryPoint", nullptr));
+	BT_NODE* pScoutMainSelector = new BT_SELECTOR("Scout", m_taskBT, 2); // 2 children
+
+	// Searching for enemy bases
+	BT_NODE* pSearchEnemyBaseSequencer = new BT_SEQUENCER("Search for enemy base", pScoutMainSelector, 2);
+	BT_NODE* pConditionNoEnemyBase = new BT_DECO_COND_LESSER_THAN("Is no known enemy base", pSearchEnemyBaseSequencer, 0, enemyBasePosListSize, false);
+	// BT_NODE* pSearchForBase = new BT_ACTION_SEARCH_NEW_ENEMY_BASE // Search base action to implement (according to Miro)
+
+	// Monitoring enemy base
+	BT_NODE* pMonitorEnemyBaseSequencer = new BT_SEQUENCER("Monitor enemy base", pScoutMainSelector, 2);
+	BT_NODE* pConditionEnemyBaseExists = new BT_DECO_COND_GREATER_THAN("Is known enemy base", pMonitorEnemyBaseSequencer, 0, enemyBasePosListSize, true);
+	// BT_NODE* pMonitorExistingBase = new BT_ACTION_MONITOR_EXISTING_BASE // Monitor action to implement (according to Miro)
+
+}
