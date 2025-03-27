58acf509694e63080e480db465d30258a7535d51
Jonas Berger <114230756+Konodinger@users.noreply.github.com>
2025-03-27 14:31:34 +0100

    Quick fix of the multiplexer, to avoid crashes in debug mode.

--- src/PMRBot/BT/BT_DECO_MULTIPLEXER.cpp
+++ src/PMRBot/BT/BT_DECO_MULTIPLEXER.cpp
@@ -1,14 +1,16 @@
 #include "BT_DECO_MULTIPLEXER.h"
-
+#include <iostream>
 
 BT_DECO_MULTIPLEXER::BT_DECO_MULTIPLEXER(std::string name, BT_NODE* parent)
-	: BT_DECORATOR(name, parent) {}
+	: BT_NODE(name, parent, 3) {}
 
 BT_NODE::State BT_DECO_MULTIPLEXER::Evaluate(void* data) {
 
-	assert(Children && ChildrenCount == 3);
-	BT_NODE::State childState = BT_DECORATOR::Evaluate(data);
-	if (Children[0]->Evaluate(data)) return ReturnState(Children[1]->Evaluate(data));
+	if (!(Children && ChildrenCount == 3)) {
+		std::cout << "Error! The Multiplexer was called but doesn't have three well-defined child.\n";
+	}
+	//BT_NODE::State childState = BT_NODE::Evaluate(data);
+	if (Children[0]->Evaluate(data) == BT_NODE::SUCCESS) return ReturnState(Children[1]->Evaluate(data));
 	else return ReturnState(Children[2]->Evaluate(data));
 }
 
--- src/PMRBot/BT/BT_DECO_MULTIPLEXER.h
+++ src/PMRBot/BT/BT_DECO_MULTIPLEXER.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <cassert>
-#include "BT_DECORATOR.h"
+#include "BT_NODE.h"
 
 // Evaluates the first child as a "conditional task". If it returns success then the second child task is run and its status is returned.
 // Else, the third child task is run and its status is returned.
 //--------------------
-class BT_DECO_MULTIPLEXER : public BT_DECORATOR {
+class BT_DECO_MULTIPLEXER : public BT_NODE {
 public:
     BT_DECO_MULTIPLEXER(std::string name, BT_NODE* parent);
     State Evaluate(void* data) override;
